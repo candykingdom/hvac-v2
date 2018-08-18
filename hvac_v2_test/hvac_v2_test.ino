642abfffdabb9444fb78d5b3dbba2baf0c357f5f
Adam Demuri <adam.demuri@gmail.com>
2018-08-17 23:46:54 -0600

    Add basic control

--- hvac_v2_test/hvac_v2_test.ino
+++ hvac_v2_test/hvac_v2_test.ino
@@ -7,59 +7,126 @@
 // https://github.com/thomasfredericks/Bounce2
 #include <Bounce2.h>
 
-
 const int kNumLeds = 3;
-const int kButtonPin = 9;
+
+const int kNapButtonPin = 9;
+const int kAuxButtonPin = 10;
 const int kSpeedPin = A2;
-const int kIntTempPin = 12;
+const int kIntTempPin = 11;
+const int kExtTempPin = 12;
+const int kWaterPin = 13;
 const int kFanPin = 6;
 const int kPumpPin = 7;
 const int kFanPwmPin = 8;
 
 CRGB leds[kNumLeds];
 
+// Temp sensors
+OneWire intOneWire(kIntTempPin);
+DallasTemperature intTemp(&intOneWire);
+OneWire extOneWire(kExtTempPin);
+DallasTemperature extTemp(&extOneWire);
 
-OneWire oneWire(kIntTempPin);
-DallasTemperature sensors(&oneWire);
-DeviceAddress insideThermometer;
-
+// Buttons
+Bounce napButton;
 Bounce auxButton;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(2, OUTPUT);
-  FastLED.addLeds<SK6812, 2>(leds, kNumLeds);
-  FastLED.showColor(CHSV(0, 255 ,255));
+  FastLED.addLeds<SK6812, 2, GRB>(leds, kNumLeds);
+  FastLED.setBrightness(16);
 
   Serial.begin(9600);
-  //pinMode(kSpeedPin, INPUT);
+  pinMode(kSpeedPin, INPUT);
   pinMode(kIntTempPin, INPUT_PULLUP);
+  pinMode(kExtTempPin, INPUT_PULLUP);
+  pinMode(kWaterPin, INPUT_PULLUP);
   pinMode(kFanPin, OUTPUT);
   pinMode(kPumpPin, OUTPUT);
   pinMode(kFanPwmPin, OUTPUT);
 
-  sensors.begin();
-  auxButton.attach(kButtonPin, INPUT_PULLUP);
-
-  digitalWrite(kFanPin, HIGH);
-  digitalWrite(kPumpPin, HIGH);
-  analogWrite(kFanPwmPin, 255);
+  intTemp.begin();
+  extTemp.begin();
+  napButton.attach(kNapButtonPin, INPUT_PULLUP);
+  auxButton.attach(kAuxButtonPin, INPUT_PULLUP);
 }
 
 
-bool on = true;
-void loop() {
-  /*digitalWrite(kFanPwmPin, HIGH);
-  delay(10);
-  digitalWrite(kFanPwmPin, LOW);
-  delay(10);
-  analogWrite(kFanPwmPin, 10);
-  digitalWrite(kFanPwmPin, HIGH);
-  delay(100);
-  digitalWrite(kFanPwmPin, LOW);
-  delay(100);*/
+// millis to run the temp read/swamp control loop at
+unsigned long tempLoopRunAt = 0;
+// TODO
+//const unsigned long kRunTempLoopEvery = 30 * 1000;
+const unsigned long kRunTempLoopEvery = 5 * 1000;
+// When it's hotter than this outside, always run the swamp cooler (even if it's cooler outside than inside).
+const float kSwampCoolerTempThresh = 70;
 
-  /*if (auxButton.update() && auxButton.read()) {
+void loop() {
+  // Make sure that we can talk to the temp sensors
+  int intDeviceCount = intTemp.getDeviceCount();
+  int extDeviceCount = extTemp.getDeviceCount();
+  if (!intDeviceCount || !extDeviceCount) {
+    Serial.println("Temp sensor missing");
+    // Flash purple
+    uint8_t brightness = (millis() / 500) % 2 ? 255 : 0;
+    leds[0] = CRGB(brightness, 0, brightness);
+    FastLED.show();
+
+    // This is important for logging
+    delay(100);
+
+    return;
+  }
+
+  float intF = intTemp.getTempFByIndex(0);
+  float extF = extTemp.getTempFByIndex(0);
+  const bool waterPresent = !digitalRead(kWaterPin);
+
+  // Don't do this check inside the temp loop, because we want to kill the pump as soon as there's no water.
+  if (!waterPresent) {
+    // Red warning light
+    leds[0] = CRGB(255, 0, 0);
+    FastLED.show();
+
+    digitalWrite(kPumpPin, LOW);
+  } else {
+    leds[0] = CRGB(0, 0, 0);
+    FastLED.show();
+  }
+  
+  if (tempLoopRunAt < millis()) {
+
+    if (intF > extF && extF < kSwampCoolerTempThresh) {
+      // Inside is hotter and it's cold outside, so just run the fan. We can still do this even if there's no water.
+      Serial.println("Running fan only");
+      digitalWrite(kFanPin, HIGH);
+      digitalWrite(kPumpPin, LOW);
+    } else if (!waterPresent) {
+      // No water and it's hot, so just turn off.
+      Serial.println("Off");
+      digitalWrite(kFanPin, LOW);
+      digitalWrite(kPumpPin, LOW);
+    } else {
+      // It's hot, so run the swamp cooler
+      Serial.println("Running swamp");
+      digitalWrite(kFanPin, HIGH);
+      digitalWrite(kPumpPin, HIGH);
+    }
+
+    tempLoopRunAt = millis() + kRunTempLoopEvery;
+  }
+
+  delay(1);
+
+
+
+
+  /*intTemp.requestTemperatures();
+  Serial.println(intTemp.getTempFByIndex(0));*/
+
+  /*analogWrite(kFanPwmPin, 10);*/
+
+  /*if (napButton.update() && napButton.read()) {
     on = !on;
     if (on) {
       analogWrite(kFanPwmPin, 255);
@@ -70,32 +137,31 @@ void loop() {
     //digitalWrite(kPumpPin, on);
     Serial.println(on);
   }*/
-  // put your main code here, to run repeatedly:
-  FastLED.showColor(CRGB(255, 255, 255));
+  /*FastLED.showColor(CRGB(0, 0, 255));
   delay(100);
-  Serial.println(millis());
+  Serial.println(millis());*/
 
   //Serial.println(millis());
   //delay(500);
-  //Serial.println(digitalRead(kButtonPin));
+  //Serial.println(digitalRead(kNapButtonPin));
   //Serial.println(analogRead(kSpeedPin));
 
   /*Serial.print("Found ");
-  Serial.print(sensors.getDeviceCount(), DEC);
+  Serial.print(intTemp.getDeviceCount(), DEC);
   Serial.println(" devices.");
    
   Serial.print("Parasite power is: "); 
-  if (sensors.isParasitePowerMode()) Serial.println("ON");
+  if (intTemp.isParasitePowerMode()) Serial.println("ON");
   else Serial.println("OFF");
 
-  if (!sensors.getAddress(insideThermometer, 0)) Serial.println("Unable to find address for Device 0"); 
+  if (!intTemp.getAddress(insideThermometer, 0)) Serial.println("Unable to find address for Device 0"); 
     Serial.print("Device 0 Address: ");
   printAddress(insideThermometer);
   Serial.println();
 
 
-  sensors.requestTemperatures();
-  Serial.println(sensors.getTempFByIndex(0));
+  intTemp.requestTemperatures();
+  Serial.println(intTemp.getTempFByIndex(0));
  
   delay(1000);*/
 }
