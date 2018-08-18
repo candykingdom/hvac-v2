b575812afa56038f78be1e07ab612328704aa8d4
Adam Demuri <adam.demuri@gmail.com>
2018-08-17 21:54:03 -0600

    Make FastLED work

--- hvac_v2_test/hvac_v2_test.ino
+++ hvac_v2_test/hvac_v2_test.ino
@@ -1,16 +1,20 @@
 #include <FastLED.h>
 
+// https://github.com/PaulStoffregen/OneWire
 #include <OneWire.h>
+// https://github.com/milesburton/Arduino-Temperature-Control-Library
 #include <DallasTemperature.h>
+// https://github.com/thomasfredericks/Bounce2
 #include <Bounce2.h>
 
 
 const int kNumLeds = 3;
 const int kButtonPin = 9;
 const int kSpeedPin = A2;
-const int kIntTempPin = 11;
+const int kIntTempPin = 12;
 const int kFanPin = 6;
 const int kPumpPin = 7;
+const int kFanPwmPin = 8;
 
 CRGB leds[kNumLeds];
 
@@ -23,31 +27,53 @@ Bounce auxButton;
 
 void setup() {
   // put your setup code here, to run once:
-  //FastLED.addLeds<WS2812, 2>(leds, kNumLeds);
-  //FastLED.showColor(CHSV(0, 255 ,255));
+  pinMode(2, OUTPUT);
+  FastLED.addLeds<SK6812, 2>(leds, kNumLeds);
+  FastLED.showColor(CHSV(0, 255 ,255));
 
   Serial.begin(9600);
   //pinMode(kSpeedPin, INPUT);
   pinMode(kIntTempPin, INPUT_PULLUP);
   pinMode(kFanPin, OUTPUT);
   pinMode(kPumpPin, OUTPUT);
+  pinMode(kFanPwmPin, OUTPUT);
 
   sensors.begin();
   auxButton.attach(kButtonPin, INPUT_PULLUP);
+
+  digitalWrite(kFanPin, HIGH);
+  digitalWrite(kPumpPin, HIGH);
+  analogWrite(kFanPwmPin, 255);
 }
 
 
-bool on = false;
+bool on = true;
 void loop() {
+  /*digitalWrite(kFanPwmPin, HIGH);
+  delay(10);
+  digitalWrite(kFanPwmPin, LOW);
+  delay(10);
+  analogWrite(kFanPwmPin, 10);
+  digitalWrite(kFanPwmPin, HIGH);
+  delay(100);
+  digitalWrite(kFanPwmPin, LOW);
+  delay(100);*/
 
-  if (auxButton.update() && auxButton.read()) {
+  /*if (auxButton.update() && auxButton.read()) {
     on = !on;
-    digitalWrite(kFanPin, on);
-    digitalWrite(kPumpPin, on);
+    if (on) {
+      analogWrite(kFanPwmPin, 255);
+    } else {
+      analogWrite(kFanPwmPin, 127);
+    }
+    //digitalWrite(kFanPin, on);
+    //digitalWrite(kPumpPin, on);
     Serial.println(on);
-  }
+  }*/
   // put your main code here, to run repeatedly:
-  //FastLED.showColor(CHSV(0, 255 ,255));
+  FastLED.showColor(CRGB(255, 255, 255));
+  delay(100);
+  Serial.println(millis());
 
   //Serial.println(millis());
   //delay(500);
@@ -72,14 +98,4 @@ void loop() {
   Serial.println(sensors.getTempFByIndex(0));
  
   delay(1000);*/
-}
-
-
-void printAddress(DeviceAddress deviceAddress)
-{
-  for (uint8_t i = 0; i < 8; i++)
-  {
-    if (deviceAddress[i] < 16) Serial.print("0");
-    Serial.print(deviceAddress[i], HEX);
-  }
 }
